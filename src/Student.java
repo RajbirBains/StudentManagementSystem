86b8c9bbc355d156ec46d82bc4b8e57565e070e0
RajbirBains <rajbir.s.bains@gmail.com>
2025-07-02 01:42:15 -0700

    Student class

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+
+    Student(String name, int year){
+
+    }
+}
