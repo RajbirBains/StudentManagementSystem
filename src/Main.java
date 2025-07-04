9b66ac13577ff747dfd5cf15a7aa68907a1872b3
RajbirBains <rajbir.s.bains@gmail.com>
2025-07-04 01:45:17 -0700

    Basic constructor

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     // Inspired by https://www.youtube.com/watch?v=GOGt7PACl10&ab_channel=MasterSkills
     public static void main(String[] args) {
         System.out.println("Student database App");
+
+        Student s1 = new Student();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private String courses;
     private int gradeYear;
     private int studentID;
+    private int tuitionBalance;
+    private int costOfCourse = 800;
 
-    Student(String name, int year){
+    public Student(){
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter Student First Name: ");
+        this.firstName = in.next();
+
+        System.out.print("Enter Student Last Name: ");
+        this.lastName = in.next();
+
+        System.out.println("1 - 1st year \n2 - 2nd year \n3 - 3rd year \n4 - 4th year \nEnter Student Grad level: ");
+        this.gradeYear = in.nextInt();
+
+        System.out.println("Name is " + this.firstName + " " + this.lastName + " and in year " + this.gradeYear);
     }
 }
